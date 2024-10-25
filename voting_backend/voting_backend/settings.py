b2ab6a1773ee9f6308d447e7212b65a675b0e3ce
Umar Sayed <sayedumar210@gmail.com>
2024-10-25 18:28:28 +0530

    added cors headers

--- voting_backend/voting_backend/settings.py
+++ voting_backend/voting_backend/settings.py
@@ -90,6 +90,7 @@ SIMPLE_JWT = {
 }
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -171,3 +172,5 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+CORS_ALLOW_ALL_ORIGINS = True
